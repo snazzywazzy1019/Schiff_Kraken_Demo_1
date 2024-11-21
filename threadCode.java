f2efe7cb37effbb501a9ec4ae73b14b1f2004235
snazzywazzy1019 <aadya.jain1019@gmail.com>
2024-11-21 12:09:19 -0500

    Added single thread
    multithread took 160s and single thread took 10s

--- threadCode.java
+++ threadCode.java
@@ -3,6 +3,8 @@ import java.util.concurrent.*;
 
 public class threadCode
 {
+	static long startTime = 0;
+
 
 	public static void main(String[] args)
 	{
@@ -10,7 +12,7 @@ public class threadCode
 		ExecutorService fixedPool = Executors.newFixedThreadPool(1000);
 
 		int totalTime = 0;
-		long startTime = System.currentTimeMillis();
+		startTime = System.currentTimeMillis();
 		try
 		{
 			for(int x = 0; x < 1000; x++)
@@ -19,7 +21,35 @@ public class threadCode
 				totalTime += future.get();
 			}
 			System.out.println(totalTime + " : total ");
-			System.out.println(" time: " + (System.currentTimeMillis() - startTime));
+			System.out.println(" time for multithread: " + (System.currentTimeMillis() - startTime));
+		}
+		catch(Exception e)
+		{
+			System.out.println("ooops");
+		}
+
+		try
+		{
+			startTime = System.currentTimeMillis();
+
+			Thread thread = new Thread()
+			{
+				public void run()
+				{
+					int numbers = 0;
+
+					for(int x = 1; x <= 1000000; x++)
+					{
+						numbers++;
+					}
+					System.out.println(" time for singlethread: " + (System.currentTimeMillis() - startTime));
+
+
+				}
+			};
+
+			thread.start();
+
 		}
 		catch(Exception e)
 		{
@@ -40,6 +70,7 @@ public class threadCode
 			return numbers;
 		}
 	}
+
 
 
 }
